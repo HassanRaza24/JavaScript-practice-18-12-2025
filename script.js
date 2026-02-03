20bc1f27a0588bdf09ba64c8ecacf4861e68aa69
Hassan Raza <Hr373073@gmail.com>
2026-02-03 21:01:56 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,82 @@
-let display = document.getElementById("screen");
+// let cond = true;
 
-let btns = document.querySelectorAll("button");
+// let prom = new Promise(function (resolve, reject) {
+//     if (cond) {
+//         resolve("work is done")
+//     } else {
+//         reject("work is fail")
+//     }
+// });
 
-btns.forEach(btn =>{
-    btn.addEventListener("click", ()=>{
-        let value = btn.innerText
-        if (value === "clear") {
-            display.value = ""
-        } else {
-            display.value = display.value + value 
-        }
+// console.log(prom)
+
+
+// function prom(cond) {
+//     return new Promise(function (resolve, reject) {
+//         if (cond) {
+//             resolve("work is done")
+//         } else {
+//             reject("work is fail")
+//         }
+//     });
+// }
+
+// console.log(prom(false))
+// console.log(prom(true))
+
+
+
+// function prom(cond) {
+//     return new Promise(function (resolve, reject) {
+//         console.log("fetching data, plz wait......!")       // pending stage
+//         setTimeout(() => {
+//             if (cond) {
+//                 resolve("work is done")                     // fulfill stage   
+//             } else {
+//                 reject("work is fail")                      // rejection stage
+//             }
+//         }, 3000);
+//     })
+// }
+
+// let onfull = (done) => {
+//     console.log(done)
+// }
+
+// let onreject = (err) => {
+//     console.log(err)
+// }
+
+// prom(false).then(onfull).catch(onreject)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function prom(cond) {
+    return new Promise(function (resolve, reject) {
+        console.log("fetching data, plz wait......!")       // pending stage
+        setTimeout(() => {
+            if (cond) {
+                resolve("work is done")                     // fulfill stage   
+            } else {
+                reject("work is fail")                      // rejection stage
+            }
+        }, 3000);
     })
+}
+
+prom(false).then((done) => {
+    console.log(done)
+}).catch((err) => {
+    console.log(err)
 })
